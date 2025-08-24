7e9803e7d8289f1c8da4407aca549e66500999dc
alameen-47 <alameenkhan1431@gmail.com>
2025-08-24 13:39:57 +0300

    emailjs Integration Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import "./index.css";
 import "./App.css";
 import { Navbar } from "./sections/Navbar.jsx";
@@ -8,11 +7,14 @@ import { Contact } from "./sections/Contact.jsx";
 import { Products } from "./sections/Products.jsx";
 import { WhyChooseUs } from "./components/whyChooseUs.jsx";
 import BigText from "./components/BigText.jsx";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
     <>
       <div className="bg-black ">
+        <Toaster position="top-center" reverseOrder={false} />
+
         <Navbar />
         <Hero />
         <Products />
--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -1,6 +1,28 @@
-import React from "react";
+import emailjs from "@emailjs/browser";
+import React, { useRef } from "react";
+import toast from "react-hot-toast";
 
 export const Contact = () => {
+  const formRef = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    emailjs
+      .sendForm("service_dw89sak", "template_q6l1l97", formRef.current, {
+        publicKey: "4qemQcnPBmkCNAES-",
+      })
+      .then(
+        (response) => {
+          toast.success("Mail Sent!!");
+          console.log("SUCCESS!", response.status, response.text);
+        },
+        (err) => {
+          toast.error("Mail send Failed!!");
+          console.log("FAILED...", err);
+        }
+      );
+  };
+
   return (
     <div className="  pb-[10rem] z-40 w-screen md:w-[50%] text-white flex justify-center align-middle items-center m-auto">
       <div className="    rounded-3xl bg-gradient-to-br from-[#00d1fb]   via-[#e402b3]  via-35% to-[#000000]  shadow-2xl shadow-white/30 md:w-[70%]  flex flex-col justify-center align-middle items-center p-[.1%] ">
@@ -11,23 +33,45 @@ export const Contact = () => {
           <span className="font-poppins font-regular text-white/50">
             We'd love to hear from you.
           </span>
-          <div className="flex flex-col justify-center align-middle items-center gap-2.5">
-            <div className="flex flex-col">
-              <label className="text-[.7rem] md:!text-[1rem]">Name</label>
-              <input className="border rounded-md md:w-[20rem] bg-white/10" />
+          <form ref={formRef} onSubmit={sendEmail}>
+            <div className="flex flex-col justify-center align-middle items-center gap-2.5">
+              <div className="flex flex-col">
+                <label className="text-[.7rem] md:!text-[1rem]">Name</label>
+                <input
+                  placeholder="Enter your name"
+                  name="name"
+                  type="text"
+                  className="border rounded-md md:w-[20rem] bg-white/10 p-[.3rem]"
+                />
+              </div>
+              <div className="flex flex-col">
+                <label
+                  name="Email"
+                  placeholder="Email"
+                  className="text-[.7rem] md:!text-[1rem]"
+                >
+                  Email
+                </label>
+                <input
+                  placeholder="Enter your Email"
+                  name="email"
+                  type="email"
+                  className="border rounded-md md:w-[20rem] bg-white/10 p-[.3rem]"
+                />
+              </div>
+              <div className="flex flex-col">
+                <label className="text-[.7rem] md:!text-[1rem]">Message</label>
+                <textarea
+                  placeholder="Enter Your Message"
+                  name="message"
+                  className="border rounded-md w-[11.5rem] md:!w-[20rem]  h-[5rem] md:h-[10rem] bg-white/10 p-[.3rem]"
+                />
+              </div>
+              <button className="text-sm md:text-xl uppercase font-reemKulfi rounded-xl border  border-blue-400 !p-[.4rem] md:!px-[5rem] tracking-wider bg-gradient-to-r  from-[#00d1fb]     to-[#000000]  hover:bg-white/30 active:scale-95 transform duration-100">
+                Submit
+              </button>
             </div>
-            <div className="flex flex-col">
-              <label className="text-[.7rem] md:!text-[1rem]">Email</label>
-              <input className="border rounded-md md:w-[20rem] bg-white/10" />
-            </div>
-            <div className="flex flex-col">
-              <label className="text-[.7rem] md:!text-[1rem]">Message</label>
-              <textarea className="border rounded-md w-[11.5rem] md:!w-[20rem]  h-[5rem] md:h-[10rem] bg-white/10" />
-            </div>
-            <button className="text-sm md:text-xl uppercase font-reemKulfi rounded-xl border  border-blue-400 !p-[.4rem] md:!px-[5rem] tracking-wider bg-gradient-to-r  from-[#00d1fb]     to-[#000000]  hover:bg-white/30 active:scale-95 transform duration-100">
-              Submit
-            </button>
-          </div>
+          </form>
         </div>
       </div>
     </div>
